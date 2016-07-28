2b46f83769deaf09836396de954c0164a3e24f00
Cédric Tissot <ctissot@artiso.com>
2016-07-28 17:55:13 +0200

    Added 4th motor

--- TxtControllerLib/Contracts/Motor.cs
+++ TxtControllerLib/Contracts/Motor.cs
@@ -5,6 +5,9 @@ namespace artiso.Fischertechnik.TxtController.Lib.Contracts
 
    public enum Motor
    {
-      One = 0, Two = 1, Three = 2
+      One = 0,
+      Two = 1,
+      Three = 2,
+      Four = 3
    }
 }
