c884e641fd41cd519ca239ade76a561fac20fa7c
Matthias Rink <mrink@artiso.com>
2016-09-16 17:57:24 +0200

    basic ui without working functions

--- RoboterApp/App.xaml.cs
+++ RoboterApp/App.xaml.cs
@@ -1,17 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using RoboterApp.Annotations;
+using RoboticsTxt.Lib.Components;
 
 namespace RoboterApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private MainWindowViewModel mainWindowViewModel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            mainWindowViewModel = new MainWindowViewModel();
+            MainWindow = new MainWindow();
+            MainWindow.DataContext = mainWindowViewModel;
+            MainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            mainWindowViewModel.Dispose();
+        }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged, IDisposable
+    {
+        private readonly ControllerSequencer controllerSequencer;
+
+        public MainWindowViewModel()
+        {
+            controllerSequencer = new ControllerSequencer();
+        }
+
+        public void Dispose()
+        {
+            controllerSequencer.Dispose();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
