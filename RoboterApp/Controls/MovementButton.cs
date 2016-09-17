e6d9b0b463c0028a959a77c7ed63046d4ec6f2a8
Matthias Rink <matthias.rink@live.com>
2016-09-17 10:49:51 +0200

    Allow three different speeds on movement

---
+++ RoboterApp/Controls/MovementButton.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using RoboticsTxt.Lib.Contracts;
+
+namespace RoboterApp.Controls
+{
+    public class MovementButton : RepeatButton
+    {
+        static MovementButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MovementButton), new FrameworkPropertyMetadata(typeof(MovementButton)));
+        }
+
+        public static readonly DependencyProperty MoveAxisCommandProperty = DependencyProperty.Register(
+            "MoveAxisCommand", typeof(ContinuousMoveAxisCommand), typeof(MovementButton), new PropertyMetadata(default(ContinuousMoveAxisCommand)));
+
+        private Speed currentSpeed;
+
+        public ContinuousMoveAxisCommand MoveAxisCommand
+        {
+            get { return (ContinuousMoveAxisCommand)GetValue(MoveAxisCommandProperty); }
+            set { SetValue(MoveAxisCommandProperty, value); }
+        }
+
+        public MovementButton()
+        {
+            currentSpeed = Speed.Fast;
+            Interval = 100;
+
+            PreviewMouseMove += OnPreviewMouseMove;
+            Click += ClickHandler;
+        }
+
+        private void OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(this);
+            var relative = position.Y / this.ActualHeight;
+
+            if (relative < 0.3)
+            {
+                currentSpeed = Speed.Slow;
+            }
+            else if (relative < 0.6)
+            {
+                currentSpeed = Speed.Quick;
+            }
+            else
+            {
+                currentSpeed = Speed.Maximal;
+            }
+        }
+
+        protected override void OnIsPressedChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnIsPressedChanged(e);
+
+            if (!IsPressed)
+            {
+                MoveAxisCommand?.OnStop();
+            }
+        }
+
+        private void ClickHandler(object sender, RoutedEventArgs e)
+        {
+            MoveAxisCommand?.OnMove(currentSpeed);
+        }
+    }
+}
--- RoboterApp/MainWindowViewModel.cs
+++ RoboterApp/MainWindowViewModel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
 using JetBrains.Annotations;
 using RoboticsTxt.Lib.Components;
 using RoboticsTxt.Lib.Contracts;
@@ -16,17 +15,17 @@ namespace RoboterApp
         {
             controllerSequencer = new ControllerSequencer();
 
-            MoveBackwardCommand = new MoveAxisCommand(controllerSequencer, Motor.Two, Movement.Left);
-            MoveForwardCommand = new MoveAxisCommand(controllerSequencer, Motor.Two, Movement.Right);
+            MoveBackwardCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Two, Movement.Left);
+            MoveForwardCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Two, Movement.Right);
 
-            MoveUpCommand = new MoveAxisCommand(controllerSequencer, Motor.Three, Movement.Left);
-            MoveDownCommand = new MoveAxisCommand(controllerSequencer, Motor.Three, Movement.Right);
+            MoveUpCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Three, Movement.Left);
+            MoveDownCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Three, Movement.Right);
 
-            TurnLeftCommand = new MoveAxisCommand(controllerSequencer, Motor.One, Movement.Left);
-            TurnRightCommand = new MoveAxisCommand(controllerSequencer, Motor.One, Movement.Right);
+            TurnLeftCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.One, Movement.Left);
+            TurnRightCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.One, Movement.Right);
 
-            OpenClampCommand = new MoveAxisCommand(controllerSequencer, Motor.Four, Movement.Left);
-            CloseClampCommand = new MoveAxisCommand(controllerSequencer, Motor.Four, Movement.Right);
+            OpenClampCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Four, Movement.Left);
+            CloseClampCommand = new ContinuousMoveAxisCommand(controllerSequencer, Motor.Four, Movement.Right);
         }
 
         public void Dispose()
@@ -34,14 +33,14 @@ namespace RoboterApp
             controllerSequencer.Dispose();
         }
 
-        public ICommand MoveBackwardCommand { get; }
-        public ICommand MoveForwardCommand { get; }
-        public ICommand MoveUpCommand { get; }
-        public ICommand MoveDownCommand { get; }
-        public ICommand TurnLeftCommand { get; }
-        public ICommand TurnRightCommand { get; }
-        public ICommand OpenClampCommand { get; }
-        public ICommand CloseClampCommand { get; }
+        public ContinuousMoveAxisCommand MoveBackwardCommand { get; }
+        public ContinuousMoveAxisCommand MoveForwardCommand { get; }
+        public ContinuousMoveAxisCommand MoveUpCommand { get; }
+        public ContinuousMoveAxisCommand MoveDownCommand { get; }
+        public ContinuousMoveAxisCommand TurnLeftCommand { get; }
+        public ContinuousMoveAxisCommand TurnRightCommand { get; }
+        public ContinuousMoveAxisCommand OpenClampCommand { get; }
+        public ContinuousMoveAxisCommand CloseClampCommand { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
