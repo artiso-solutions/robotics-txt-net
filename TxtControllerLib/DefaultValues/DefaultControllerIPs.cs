099330929b869e238edac7a2f1d14cf4461d500f
Lukas Eisele <leisele@artiso.com>
2016-11-04 14:31:55 +0100

    added default controller IPs

---
+++ TxtControllerLib/DefaultValues/DefaultControllerIPs.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace RoboticsTxt.Lib.DefaultValues
+{
+    public static class DefaultControllerIPs
+    {
+        public static IPAddress DefaultUSBAddress => IPAddress.Parse("192.168.7.2");
+
+        public static IPAddress DefaultWLanAddress => IPAddress.Parse("192.168.8.2");
+
+        public static IPAddress DefaultBluetoothAddress => IPAddress.Parse("192.168.9.2");
+    }
+}
