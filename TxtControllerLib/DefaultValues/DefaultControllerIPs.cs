4278eff95416c671cb1f345f14c80230f4daf98e
Lukas Eisele <leisele@artiso.com>
2016-11-04 15:00:29 +0100

    added xml documentation for default values changed target framework to 4.6

--- TxtControllerLib/DefaultValues/DefaultControllerIPs.cs
+++ TxtControllerLib/DefaultValues/DefaultControllerIPs.cs
@@ -2,12 +2,24 @@
 
 namespace RoboticsTxt.Lib.DefaultValues
 {
+    /// <summary>
+    /// Default values for the IP addresses used by the "Robotics TXT" controller.
+    /// </summary>
     public static class DefaultControllerIPs
     {
+        /// <summary>
+        /// IP used for a USB connection.
+        /// </summary>
         public static IPAddress DefaultUSBAddress => IPAddress.Parse("192.168.7.2");
 
+        /// <summary>
+        /// IP used for a WLan connection.
+        /// </summary>
         public static IPAddress DefaultWLanAddress => IPAddress.Parse("192.168.8.2");
 
+        /// <summary>
+        /// IP used for a Bluetooth connection.
+        /// </summary>
         public static IPAddress DefaultBluetoothAddress => IPAddress.Parse("192.168.9.2");
     }
 }
